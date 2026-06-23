7aea33df6fa349c5be1d372cb5708921625e5cb5
Allan Silva <allandasilva33@outlook.com>
2026-06-23 00:57:54 -0300

    feat: adiciona background em ContactSection

--- src/components/ui/main/contact-section/ContactSection.tsx
+++ src/components/ui/main/contact-section/ContactSection.tsx
@@ -1,12 +1,19 @@
+import clsx from "clsx";
 import { copyContactSummary } from "../../../../data/section-summary.copy";
 import SectionSummary from "../../common/section/SectionSummary";
+import SectionBackground from "../../common/section/SectionBackground";
 import Contacts from "./contacts/Contacts";
 import styles from "./ContactSection.module.css";
 
 const ContactSection = () => {
   return (
     <section className={styles.contact_section}>
-      <div className={"max_width_container"}>
+      <SectionBackground>
+        <div className={styles.background_max_width}>
+          <div></div>
+        </div>
+      </SectionBackground>
+      <div className={clsx("max_width_container", styles.content_wrapper)}>
         <SectionSummary
           sectionCopy={copyContactSummary}
           className={styles.section_summary}
