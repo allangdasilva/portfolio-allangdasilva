2d9bcb59203b943844aaf083d7b909f8883bb9d7
Allan Silva <allandasilva33@outlook.com>
2026-06-22 23:22:38 -0300

    feat: adiciona background em TrainingSection

--- src/components/ui/main/training-section/TrainingSection.tsx
+++ src/components/ui/main/training-section/TrainingSection.tsx
@@ -1,12 +1,19 @@
+import clsx from "clsx";
 import { copyTrainingsSummary } from "../../../../data/section-summary.copy";
 import SectionSummary from "../../common/section/SectionSummary";
 import Trainings from "./trainings/Trainings";
 import styles from "./TrainingSection.module.css";
+import SectionBackground from "../../common/section/SectionBackground";
 
 const TrainingSection = () => {
   return (
     <section className={styles.trainings_section}>
-      <div className={"max_width_container"}>
+      <SectionBackground>
+        <div className={styles.background_max_width}>
+          <div></div>
+        </div>
+      </SectionBackground>
+      <div className={clsx("max_width_container", styles.content_wrapper)}>
         <SectionSummary
           sectionCopy={copyTrainingsSummary}
           className={styles.section_summary}
