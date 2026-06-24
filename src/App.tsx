8b484cd747f61b3d825e2a6eb2e135b5f7158b9b
Allan Silva <allandasilva33@outlook.com>
2026-06-24 00:12:43 -0300

    feat: adiciona componente de Loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Footer from "./components/layout/footer/Footer";
 import Header from "./components/layout/header/Header";
 import Main from "./components/layout/main/Main";
+import Loader from "./components/ui/loader/Loader";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <Main />
       <Footer />
+      <Loader />
     </>
   );
 }
