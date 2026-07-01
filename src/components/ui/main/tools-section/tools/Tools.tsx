822ca928fa75bb8c036efdee87587eb1de24b297
Allan Silva <allandasilva33@outlook.com>
2026-06-30 23:18:52 -0300

    feat: adiciona efeito de motion na sessão Tools

--- src/components/ui/main/tools-section/tools/Tools.tsx
+++ src/components/ui/main/tools-section/tools/Tools.tsx
@@ -1,21 +1,56 @@
+import { motion, useInView } from "motion/react";
+import { useRef } from "react";
 import { toolsCopy } from "../../../../../data/tools.copy";
 import ToolsCard from "./tools-card/ToolsCard";
 import styles from "./Tools.module.css";
 
 const Tools = () => {
+  const toolsCopyLeft = toolsCopy.slice(0, 3);
+  const toolsCopyRight = toolsCopy.slice(-3);
+
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const wrapperVariants = {
+    initial: {},
+    animate: {
+      transition: {
+        staggerChildren: 0.1,
+      },
+    },
+  };
+
   return (
-    <div className={styles.tools_wrapper}>
-      <div className={styles.tools_left}>
-        {toolsCopy.slice(0, 3).map((card) => (
-          <ToolsCard key={card.id} card={card} />
+    <motion.div
+      ref={ref}
+      variants={wrapperVariants}
+      initial={"initial"}
+      animate={isInView ? "animate" : ""}
+      className={styles.tools_wrapper}
+    >
+      {/* Left */}
+      <motion.div className={styles.tools_left}>
+        {toolsCopyLeft.slice(0, 3).map((card, i) => (
+          <ToolsCard
+            key={card.id}
+            card={card}
+            position="left"
+            isLast={i + 1 === toolsCopyLeft.length}
+          />
         ))}
-      </div>
-      <div className={styles.tools_right}>
-        {toolsCopy.slice(-3).map((card) => (
-          <ToolsCard key={card.id} card={card} />
+      </motion.div>
+      {/* Right */}
+      <motion.div className={styles.tools_right}>
+        {toolsCopyRight.slice(-3).map((card, i) => (
+          <ToolsCard
+            key={card.id}
+            card={card}
+            position="right"
+            isLast={i + 1 === toolsCopyRight.length}
+          />
         ))}
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
 
--- src/components/ui/main/tools-section/tools/tools-card/ToolsCard.tsx
+++ src/components/ui/main/tools-section/tools/tools-card/ToolsCard.tsx
@@ -1,28 +1,78 @@
+import { motion, type HTMLMotionProps, type Variants } from "motion/react";
 import ItemHeading from "../../../../common/ItemHeading";
 import List from "../../../../common/list/List";
 import ListItem from "../../../../common/list/ListItem";
 import styles from "./ToolsCard.module.css";
+import clsx from "clsx";
 
-type Props = React.HTMLAttributes<HTMLDivElement> & {
+type Props = HTMLMotionProps<"div"> & {
   card: {
     id: number;
     title: string;
     tools: string[];
   };
+  position: "left" | "right";
+  isLast: boolean;
 };
 
-const ToolsCard = ({ card }: Props) => {
+const ToolsCard = ({ card, position, isLast }: Props) => {
   const { title, tools } = card;
 
+  const cardVariants: Variants = {
+    initial: {
+      opacity: 0,
+    },
+    animate: {
+      opacity: 1,
+      transition: {
+        duration: 0.4,
+      },
+    },
+  };
+  const detailVariants: Variants = {
+    initial: {
+      clipPath: "polygon(0 0, 100% 0, 100% 0, 0 0)",
+    },
+    animate: {
+      clipPath: "polygon(0 0, 100% 0, 100% 120%, 0 120%)",
+      transition: {
+        duration: 0.8,
+      },
+    },
+  };
+
   return (
-    <div className={styles.card_wrapper}>
-      <ItemHeading>{title}</ItemHeading>
+    <motion.div
+      className={clsx(
+        styles.card_wrapper,
+        position === "left" && styles.card_wrapper_left,
+        position === "right" && styles.card_wrapper_right,
+      )}
+    >
+      {/* Detail */}
+      <motion.div
+        variants={detailVariants}
+        aria-hidden="true"
+        className={styles.detail}
+      >
+        <motion.div className={styles.circ}></motion.div>
+        <motion.div
+          className={clsx(styles.dashed, isLast && styles.dashed_none)}
+        ></motion.div>
+      </motion.div>
+      {/* Title */}
+      <motion.div variants={cardVariants}>
+        <ItemHeading>{title}</ItemHeading>
+      </motion.div>
+      {/* List */}
       <List>
         {tools.map((tool) => (
-          <ListItem key={tool}>{tool}</ListItem>
+          <motion.div key={tool} variants={cardVariants}>
+            <ListItem>{tool}</ListItem>
+          </motion.div>
         ))}
       </List>
-    </div>
+    </motion.div>
   );
 };
 
