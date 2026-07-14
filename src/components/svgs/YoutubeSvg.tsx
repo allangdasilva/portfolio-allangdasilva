66787f27a4f589ab8c612bd0591ad714bf3b336e
Allan Silva <allandasilva33@outlook.com>
2026-07-13 23:15:59 -0300

    fix: corrige códigos desnecessários dos SVG's

--- src/components/svgs/YoutubeSvg.tsx
+++ src/components/svgs/YoutubeSvg.tsx
@@ -7,19 +7,12 @@ const YoutubeSvg = () => {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g clipPath="url(#clip0_216_657)">
-        <path
-          fillRule="evenodd"
-          clipRule="evenodd"
-          d="M39.0625 39.0625V0.9375H0.9375V39.0625H39.0625ZM16 25.0675V13.0225L26.3925 19.0475L16 25.0675Z"
-          fill="var(--color-text-yellow-500)"
-        />
-      </g>
-      <defs>
-        <clipPath id="clip0_216_657">
-          <rect width="40" height="40" fill="white" />
-        </clipPath>
-      </defs>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M39.0625 39.0625V0.9375H0.9375V39.0625H39.0625ZM16 25.0675V13.0225L26.3925 19.0475L16 25.0675Z"
+        fill="var(--color-text-yellow-500)"
+      />
     </svg>
   );
 };
