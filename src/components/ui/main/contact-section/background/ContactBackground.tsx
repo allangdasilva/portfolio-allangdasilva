768c487037bb3f9a13dc4fccf62053b554f4e2e7
Allan Silva <allandasilva33@outlook.com>
2026-07-13 22:26:51 -0300

    feat: adiciona efeito de motion ao background de Contact

--- src/components/ui/main/contact-section/background/ContactBackground.tsx
+++ src/components/ui/main/contact-section/background/ContactBackground.tsx
@@ -1,11 +1,32 @@
+import { motion, type Variants } from "motion/react";
 import SectionBackground from "../../../common/section/SectionBackground";
 import styles from "./ContactBackground.module.css";
+import { useRef } from "react";
+import { useInView } from "motion/react";
 
 const ContactBackground = () => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: false });
+
+  const backgroundVariants: Variants = {
+    initial: {
+      opacity: 0,
+    },
+    animate: {
+      opacity: 1,
+      transition: { duration: 0.6, ease: "easeOut" },
+    },
+  };
+
   return (
     <SectionBackground>
-      <div className={styles.background_container}>
-        <div className={styles.background_image}></div>
+      <div ref={ref} className={styles.background_container}>
+        <motion.div
+          variants={backgroundVariants}
+          initial="initial"
+          animate={isInView ? "animate" : ""}
+          className={styles.background_image}
+        ></motion.div>
       </div>
     </SectionBackground>
   );
