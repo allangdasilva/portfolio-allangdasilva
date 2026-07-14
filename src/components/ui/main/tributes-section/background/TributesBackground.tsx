37583ee5193f0eb9686c545491c2206942d035e1
Allan Silva <allandasilva33@outlook.com>
2026-07-13 21:43:15 -0300

    remove: remove animação do mockingjayLogo

--- src/components/ui/main/tributes-section/background/TributesBackground.tsx
+++ src/components/ui/main/tributes-section/background/TributesBackground.tsx
@@ -1,9 +1,4 @@
-import {
-  motion,
-  useMotionValueEvent,
-  useScroll,
-  type Variants,
-} from "motion/react";
+import { useMotionValueEvent, useScroll, type Variants } from "motion/react";
 import { useRef, useState } from "react";
 import SectionBackground from "../../../common/section/SectionBackground";
 import HandOneSvg from "../../../../svgs/HandOneSvg";
@@ -23,24 +18,12 @@ const TributesBackground = () => {
   useMotionValueEvent(scrollYProgress, "change", (latest) => {
     let currentStep = 0;
 
-    if (latest >= 0.8) currentStep = 4;
-    else if (latest >= 0.75) currentStep = 3;
-    else if (latest >= 0.7) currentStep = 2;
-    else if (latest >= 0.3) currentStep = 1;
+    if (latest >= 0.8) currentStep = 3;
+    else if (latest >= 0.75) currentStep = 2;
+    else if (latest >= 0.7) currentStep = 1;
 
     setAnimationStep((prev) => (prev !== currentStep ? currentStep : prev));
   });
-
-  const mockingjayVariants: Variants = {
-    offscreen: {
-      opacity: 0.4,
-      transition: { duration: 0.8, ease: "easeIn" },
-    },
-    onscreen: {
-      opacity: 0.8,
-      transition: { duration: 0.8, ease: "easeOut" },
-    },
-  };
 
   const handVariants: Variants = {
     offscreen: {
@@ -65,31 +48,26 @@ const TributesBackground = () => {
   return (
     <SectionBackground>
       <div ref={ref} className={styles.background_container}>
-        <motion.div
-          variants={mockingjayVariants}
-          initial="offscreen"
-          animate={animationStep >= 1 ? "onscreen" : "offscreen"}
-          className={styles.mockingjay_graffiti}
-        ></motion.div>
+        <div className={styles.mockingjay_graffiti}></div>
         <div className={styles.hands_wrapper}>
           <HandOneSvg
             variants={handVariants}
             initial="offscreen"
-            animate={animationStep >= 2 ? "onscreen" : "offscreen"}
+            animate={animationStep >= 1 ? "onscreen" : "offscreen"}
           />
 
           <HandTwoSvg
             variants={handVariants}
             custom={-144}
             initial="offscreen"
-            animate={animationStep >= 3 ? "onscreen" : "offscreen"}
+            animate={animationStep >= 2 ? "onscreen" : "offscreen"}
           />
 
           <HandThreeSvg
             variants={handVariants}
             custom={116}
             initial="offscreen"
-            animate={animationStep >= 4 ? "onscreen" : "offscreen"}
+            animate={animationStep >= 3 ? "onscreen" : "offscreen"}
           />
         </div>
       </div>
